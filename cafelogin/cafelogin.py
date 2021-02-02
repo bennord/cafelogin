c4aebc9362ecb93b31d097a82b95f5db094e1bf1
bennord <ben.nordstrom@gengo.com>
2021-02-02 19:10:11 +0900

    wait for usrl changes

--- cafelogin/cafelogin.py
+++ cafelogin/cafelogin.py
@@ -1,15 +1,22 @@
 import requests
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
 from typing import Sequence
 
 DETECT_PORTAL_URL = "http://detectportal.firefox.com/"
+
+
+def wait_url_change(url: str, driver: WebDriver):
+    wait = WebDriverWait(driver, 3)
+    wait.until(expected_conditions.url_changes(url))
+    url = driver.current_url
+    print(f"Url: {url}")
+    return url
 
 
 def portal_connected():
@@ -23,16 +30,16 @@ def try_portal_login():
             version="v0.29.0", cache_valid_range=100
         ).install()
     ) as driver:
-        wait = WebDriverWait(driver, 3)
+        url = driver.current_url
         print(f"Nav to: {DETECT_PORTAL_URL}")
         driver.get(DETECT_PORTAL_URL)
-        print(f"Url: {driver.current_url}")
+        url = wait_url_change(url, wait)
         print(f"Click button_next_page")
         driver.find_element_by_id("button_next_page").click()
-        print(f"Url: {driver.current_url}")
+        url = wait_url_change(url, wait)
         print(f"Click button_accept")
         driver.find_element_by_id("button_accept").click()
-        print(f"Url: {driver.current_url}")
+        url = wait_url_change(url, wait)
 
 
 def ensure_portal_connection():
