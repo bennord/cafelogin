c5c36667ce1f89eda4b8c064deadb189570e1b47
bennord <ben.nordstrom@gengo.com>
2021-02-02 18:01:22 +0900

    add portal detection

--- wifi_portal_tools/__main__.py
+++ wifi_portal_tools/__main__.py
@@ -1,3 +1,4 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
@@ -8,18 +9,43 @@ from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
 from typing import Sequence
 
+DETECT_PORTAL_URL = "http://detectportal.firefox.com/"
 
-with webdriver.Firefox(executable_path=GeckoDriverManager().install()) as driver:
-    wait = WebDriverWait(driver, 3)
-    driver.get("https://service.wi2.ne.jp/freewifi/starbucks/index.html")
-    print(driver.current_url)
-    driver.find_element_by_id("button_next_page").click()
-    print(driver.current_url)
-    driver.find_element_by_id("button_accept").click()
-    wait.until(
-        expected_conditions.presence_of_all_elements_located(
-            (By.CLASS_NAME, "main_img")
-        )
-    )
-    print(driver.current_url)
-    print("SUCCESS")
+
+def portal_connected():
+    response = requests.get(DETECT_PORTAL_URL)
+    return response.ok and response.text.strip() == "success"
+
+
+def try_portal_login():
+    with webdriver.Firefox(
+        executable_path=GeckoDriverManager(
+            version="v0.29.0", cache_valid_range=100
+        ).install()
+    ) as driver:
+        wait = WebDriverWait(driver, 3)
+        print(f"Nav to: {DETECT_PORTAL_URL}")
+        driver.get(DETECT_PORTAL_URL)
+        print(f"Url: {driver.current_url}")
+        print(f"Click button_next_page")
+        driver.find_element_by_id("button_next_page").click()
+        print(f"Url: {driver.current_url}")
+        print(f"Click button_accept")
+        driver.find_element_by_id("button_accept").click()
+        print(f"Url: {driver.current_url}")
+
+
+def ensure_portal_connection():
+    if portal_connected():
+        print("Already connected")
+        return
+
+    try_portal_login()
+
+    if portal_connected():
+        print("Login success")
+    else:
+        print("Login failed")
+
+
+ensure_portal_connection()
