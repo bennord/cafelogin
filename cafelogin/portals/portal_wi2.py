eabedea260d0d57f752f998cdac0af37d356b604
bennord <ben.nordstrom@gengo.com>
2021-05-10 15:50:49 +0900

    update for optional "next" page on wi2 portal

--- cafelogin/portals/portal_wi2.py
+++ cafelogin/portals/portal_wi2.py
@@ -1,14 +1,25 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
+
 from .util import wait_url_change
 
 
 def login(driver: WebDriver):
     url = driver.current_url
-    print(f"Click button_next_page")
-    driver.find_element_by_id("button_next_page").click()
-    url = wait_url_change(url, driver)
-    print(f"Url: {url}")
-    print(f"Click button_accept")
-    driver.find_element_by_id("button_accept").click()
-    url = wait_url_change(url, driver)
-    print(f"Url: {url}")
+    try:
+        print(f"Find button_next_page")
+        button_next_page = driver.find_element_by_id("button_next_page")
+        button_next_page.click()
+        url = wait_url_change(url, driver)
+        print(f"Url: {url}")
+    except NoSuchElementException:
+        print(f"Not found: button_next_page")
+
+    try:
+        print(f"Find button_accept")
+        button_accept = driver.find_element_by_id("button_accept")
+        button_accept.click()
+        url = wait_url_change(url, driver)
+        print(f"Url: {url}")
+    except NoSuchElementException:
+        print(f"Not found: button_accept")
